7da4d9ea58c867c7d751e5876ec5b08c3b1940b8
ketongwang <kentwang16@gmail.com>
2014-10-05 23:52:39 -0500

    none

--- concept_learning/find_S.m
+++ concept_learning/find_S.m
@@ -2,3 +2,19 @@
 
 %- data representation (data_raw, name, varLevel)
 data = cat_data_represent(data_raw, varLevel);
+
+[n, p] = size(data);
+
+h = cell(1, p); % cell of most specific cos
+for i = 1:n
+  if data(i, 1) != 1
+    continue;
+   else % for each positive traning example
+    for j = 1:p % for each attribute
+      if sum(ismember(h{j}, data(i, j))) > 1
+        continue;
+      else
+        h{j} = constraint_generalizer(h{j}, data(i, j), varLevelNum{j})
+      endif
+    endfor
+   endif
