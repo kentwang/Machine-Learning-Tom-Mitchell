a5c8e9e4dcaa725049a1605bbbf00b979812e393
ketongwang <kentwang16@gmail.com>
2014-10-06 12:43:31 -0500

    Constraint generalizer function done

--- concept_learning/constraint_generalizer.m
+++ concept_learning/constraint_generalizer.m
@@ -1,2 +1,6 @@
-function hj_new = constraint_generalizer(hj, attr, attrLevels)
-  if hj == 0
+function hj = constraint_generalizer(hj, attr, attrLevels)
+  if sum(size(hj)) == 0 % most specific constraint
+    hj = attr;
+  else
+    hj = attrLevels;
+  endif
